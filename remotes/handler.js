f14352b52bd7ede4552cd2470d5438edd05d1140
Blaine Price <1wbprice@gmail.com>
2017-07-14 16:16:38 -0400

    Move other stuff over.

--- remotes/handler.js
+++ remotes/handler.js
@@ -1,16 +1,14 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+/**
+ * @module users 
+ * @description
+ * Exposes REST actions for dealing with user records.
+ */
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports = {
+    create: require('./lib/create'),
+    read: require('./lib/read'),
+    update: require('./lib/update'),
+    delete: require('./lib/delete')
 };
