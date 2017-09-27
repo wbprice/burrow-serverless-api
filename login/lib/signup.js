44933c0d02b3a2e815f5269f2a20697b49bc0a64
Blaine Price <1wbprice@gmail.com>
2017-09-27 15:43:03 -0400

    Implement login.

--- login/lib/signup.js
+++ login/lib/signup.js
@@ -1,7 +1,42 @@
 'use strict';
 
-function signup() {
+const { 
+    CognitoUserPool,
+    CognitoUserAttribute,
+    CognitoUser
+} = require('amazon-cognito-identity-js');
 
+const UserPoolId = process.env.USER_POOL_ID;
+const ClientId = process.env.CLIENT_ID;
+
+function signup(event, context, callback) {
+
+    if (event.headers["Content-Type"] !== 'application/json') {
+        return callback(new Error('Wrong content type'));
+    }
+
+    const userPool = CognitoUserPool({
+        UserPoolId,
+        ClientId
+    })
+
+    const attributeList = [
+        CognitoUserAttribute({
+            Name: 'name',
+            Value: data.name
+        }),
+        CognitoUserAttribute({
+            Name: 'email',
+            Value: data.email'
+        })
+    ];
+
+    return userPool.signup(data.emailAddress, data.password, attributeList, null, (err, result) => {
+        if (err) {
+            return callback(err);
+        } 
+        return callback(null, result.user);
+    });
 }
 
 module.exports = signup;
