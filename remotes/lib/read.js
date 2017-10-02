ba0df4a1b396af804f00559154be530b0a72191e
Blaine Price <1wbprice@gmail.com>
2017-10-02 15:59:42 -0400

    Let's start breaking apart the remotes api.

--- remotes/lib/read.js
+++ remotes/lib/read.js
@@ -1,23 +1,12 @@
 'use strict';
 
-const dbClient = require('./dbClient');
+const AWS = require('aws-sdk');
 
-function list(callback) {
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE
-    }
-
-    return dbClient.scan(params, (error, result) => {
-        if (error) {
-            return callback(error);
-        }
-
-        return callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(result.Items)
-        });
-    });
-}
+const { 
+        USER_POOL_ID, 
+        CLIENT_ID, 
+        IDENTIY_POOL_ID 
+      } = process.env;
 
 function readOne(id, callback) {
     const params = {
@@ -80,4 +69,38 @@ function read(event, context, callback) {
     return list(callback);
 }
 
-module.exports = read;
+function list(event, context, callback) {
+    const token = event.headers.Authorization && event.headers.Authorization.replace('Bearer ', ''); 
+
+    if (!token) {
+        return callback(null, {
+            statusCode: 403,
+            body: JSON.stringify('A bearer token is required to access this resource');
+        });
+    }
+
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId : IDENTIY_POOL_ID,
+        Logins : {
+            [`cognito-idp.${AWS_REGION}.amazonaws.com/${USER_POOL_ID}`] : token
+        }
+    });
+
+    const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE
+    }
+
+    return dbClient.scan(params, (error, result) => {
+        if (error) {
+            return callback(error);
+        }
+
+        return callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(result.Items)
+        });
+    });
+}
+
+module.exports = list;
